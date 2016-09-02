358b787cd096046d1e8dd61983a94eda0a2f5a80
George Novik <grgnvk@gmail.com>
2016-09-02 08:07:02 +0300

    First commit

---
+++ DependencyInjection/TalakaCarrotQuestExtension.php
@@ -0,0 +1,35 @@
+<?php
+
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+
+use Symfony\Component\DependencyInjection\Container;
+use Symfony\Component\DependencyInjection\Exception\BadMethodCallException;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\Config\FileLocator;
+
+/**
+ * Created by PhpStorm.
+ * User: fliak
+ * Date: 2.9.16
+ * Time: 7.51
+ */
+class TalakaCarrotQuestExtension extends Extension
+{
+
+
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
+
+    }
+
+
+}
---
+++ TalakaCarrotQuestBundle.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fliak
+ * Date: 2.9.16
+ * Time: 6.41
+ */
+
+namespace Talaka\CarrotQuestBundle;
+
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TalakaCarrotQuestBundle extends Bundle
+{
+
+}
